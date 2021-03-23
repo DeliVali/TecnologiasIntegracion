7f88e99a1a70d6d0dc8b600f6ee4cf6841877072
DeliVali <75693611+DeliVali@users.noreply.github.com>
2021-03-23 17:01:30 -0600

    Add files via upload

--- Actividad 3 y 4/cliente-soap/soap.js
+++ Actividad 3 y 4/cliente-soap/soap.js
@@ -1,22 +1,36 @@
+var mensaje
+function ini(){
+    mensaje = '<?xml version="1.0" encoding="utf-8"?>' +
+    '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
+    '<Body>' +
+    '<SumarRequest xmlns="http://www.example.org/calculadora">' +
+    '<a>'+document.getElementById('a').value+'</a>' +
+    '<b>'+document.getElementById('b').value+'</b>' +
+    '</SumarRequest>' +
+    '</Body>' +
+    '</Envelope>';
+}
 
-var mensaje = '<?xml version="1.0" encoding="utf-8"?>' +
-'<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/' +
-'<Body>' +
-'<SumarRequest>' +
-'<element name="a" type="int">' +
-'<element name="b" type="int">' +
-'</SumarRequest>' +
-'</Body>' +
-'</Envelope>';
+
 
 
 function soap() {
     // alert('hola')
-    axios.post('http://localhost:8080/ws/calculadora', mensaje,{
+    ini()
+    axios.post('http://localhost:8080/ws/calculadora/', mensaje,{
         headers:{
             'Content-Type' : 'text/xml'
         }
     })
-    .then(response => console.log(response.data))
+    .then(function (response){
+document.getElementById('r').value=resultado(response.data)
+    })
     .catch(err => console.log(err));
+}
+
+function resultado(rXml){
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(rXml,"text/xml");
+    var resul = xmlDoc.getElementsByTagName("ns2:resultado")[0].childNodes[0].nodeValue;
+    return resul;
 }
